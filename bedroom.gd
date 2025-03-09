4611b680384fb1519816ec4fee17ae5fd2453642
maxinesofia <maxinesofia.education@gmail.com>
2025-03-10 00:53:45 +0800

    added fix closet

--- bedroom.gd
+++ bedroom.gd
@@ -16,11 +16,11 @@ func _on_back_button_pressed() -> void:
 
 
 func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Options again.tscn")
+	get_tree().change_scene_to_file("res://Options in bedroom.tscn")
 
 
 func _on_x_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://1.tscn")
+	get_tree().change_scene_to_file("res://bedroom.tscn")
 
 
 func _on_exit_pressed() -> void:
@@ -42,4 +42,4 @@ func bed_on_button_pressed() -> void:
 
 
 func closet_on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://closet.tscn")
+	get_tree().change_scene_to_file("res://fixing closet.tscn")
